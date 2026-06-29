382698641ffab4333cbd6b8f3e39fbfab77f0382
jeffmart33 <jeffmartins172@gmail.com>
2026-06-29 16:24:00 -0700

    Refactor sendEmail to use SMTP with timeouts
    Updated email transport configuration to use SMTP settings and added timeout options.

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -3,16 +3,18 @@ import nodemailer from "nodemailer";
 const sendEmail = async (to, subject, html) => {
   try {
     const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
 
-  tls: {
-    rejectUnauthorized: false,
-  },
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
+  socketTimeout: 30000,
 });
 
     const mailOptions = {
