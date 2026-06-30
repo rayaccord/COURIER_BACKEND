23d58db04f12a9e89982989e2493f0c960e5ab83
jeffmart33 <jeffmartins172@gmail.com>
2026-06-30 14:33:57 -0700

    Increase max distance for pickup location search

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -37,7 +37,7 @@ const nearbyCouriers =
     location: {
       $near: {
         $geometry: order.pickupLocation,
-        $maxDistance: 50000,
+        $maxDistance: 5000000,
       },
     },
   });
